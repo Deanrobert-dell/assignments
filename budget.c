bec5e77c20f1636d96f19e42c2e12c384ca9be0f
Deanrobert-dell <dean.peterson@ucas-edu.net>
2024-09-12 19:41:08 +0000

    jgfyrhjguygcfgh

--- budget.c
+++ budget.c
@@ -1,7 +1,13 @@
 int main(void){
-    double income[7];
-    printf("this is budget calc \nhow much do you make ");
-    scanf("%lf", income);
-    printf("youmake %lf", income);
+    char income[7];
+    char utilities[9];
+    char groceries[9];
+    char car[9];
+    printf("this is budget calc \nhow much do you make\n");
+    scanf("%s", income);
+    printf("how much do yor utilities cost ");
+    scanf("%s", utilities);
+    printf("youmake %s", income);
+    float 
     return 0;
 }
