d8cb8779b15ecf8f5a679fad0efdb57e8d778f99
Deanrobert-dell <dean.peterson@ucas-edu.net>
2024-09-12 23:08:05 +0000

    bjvhbu

---
+++ expression.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    double a = 6;
+    double b = 24;
+
+    double result1 = 7 - (b / 8 * 4) + 6;
+    printf("Result 1: %f\n", result1);
+
+    double result2 = (18 / 3) - 7 + (2 * 5);
+    printf("Result 2: %f\n", result2);
+
+    double result3 = (a * 4 / 12) + (72 / 8) - 9;
+    printf("Result 3: %f\n", result3);
+
+    double result4 = (17 - (a / 2)) + (4 * 3);
+    printf("Result 4: %f\n", result4);
+
+    double result5 = -2 * (1 * 4 - (a / 2)) + (6 + 2 - 3);
+    printf("Result 5: %f\n", result5);
+
+    double result6 = -1 * ((3 - (b / 7)) / 5) - (2 * 24 / 6);
+    printf("Result 6: %f\n", result6);
+
+    double result7 = (3 * (b / 5) / 15) - (5 - (2 * 2));
+    printf("Result 7: %f\n", result7);
+
+    double result8 = (1 * (b / 12) + (3 * 3 * 3)) - (2 * 2 * 2 * 2 * 2) / 4;
+    printf("Result 8: %f\n", result8);
+
+    double result9 = pow((22 / 2 - 2 * 5), 2) + pow((4 - (6 / 6)), 2);
+    printf("Result 9: %f\n", result9);
+
+    return 0;
+}
--- test.c
+++ test.c
@@ -4,3 +4,4 @@ int main(void) {
     printf("world");
     return 0;
 }
+dont copy
