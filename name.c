f303867c3d2a9dff9bf9c125b75d7c5f72ea792c
Deanrobert-dell <dean.peterson@ucas-edu.net>
2024-09-20 19:40:36 +0000

    hgygyg

--- name.c
+++ name.c
@@ -2,11 +2,15 @@
 
 int main() {
     char name[100];
+    printf("Enter your name: ");
+    fgets(name, sizeof(name), stdin);
     
-    printf("insert name\n");
-    scanf("%99s", name);
-    printf("<<< %s >>>\n", name);
-    
+    // this took me half hour
+    name[strcspn(name, "\n")] = 0;
+//ihate this
+    printf("*.* %s *.*\n", name);
     return 0;
 }
+
+
 
--- name.py
+++ name.py
@@ -1,3 +1,3 @@
-print("insert name")
-name=input()
-print("<<<"&name&">>>")
+name = input("Enter your name: ")
+decoratedName = f"*.* {name} *.*"
+print(decoratedName)
