77fb04c7e707e0972072d18d2aea2ad7bea66db2
Deanrobert-dell <dean.peterson@ucas-edu.net>
2024-09-24 19:02:16 +0000

    bad

---
+++ silly.py
@@ -0,0 +1,7 @@
+# All inputs
+noun = input("Choose a noun: ")
+p_verb = input("Choose a verb: ")
+noun2 = input("Choose a noun: ")
+# Displays the story based input
+
+print ("lick your "+ noun +" and it will "+ p_verb +" you, use your "+ noun2 +" to block it")
--- test.c
+++
@@ -1,7 +0,0 @@
-
-
-int main(void) {
-    printf("world");
-    return 0;
-}
-dont copy h
