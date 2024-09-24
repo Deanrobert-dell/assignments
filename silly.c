555181281c15afd5edfce39f37da126e802febda
Deanrobert-dell <dean.peterson@ucas-edu.net>
2024-09-24 19:06:02 +0000

    bad2

---
+++ silly.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    char noun[100];
+    char p_verb[100];
+    char noun2[100];
+
+    printf("Choose a noun: ");
+    scanf("%s", noun);
+    printf("Choose a verb: ");
+    scanf("%s", p_verb);
+    printf("Choose a noun: ");
+    scanf("%s", noun2);
+
+    printf("lick your %s and it will %s you, use your %s to block it\n", noun, p_verb, noun2);
+
+    return 0;
+}
--- silly.py
+++ silly.py
@@ -5,3 +5,13 @@ noun2 = input("Choose a noun: ")
 # Displays the story based input
 
 print ("lick your "+ noun +" and it will "+ p_verb +" you, use your "+ noun2 +" to block it")
+
+
+
+
+noun = input("Choose a noun: ")
+p_verb = input("Choose a verb: ")
+noun2 = input("Choose a noun: ")
+
+print(f"lick your {noun} and it will {p_verb} you, use your {noun2} to block it")
+
