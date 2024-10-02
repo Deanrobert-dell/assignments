5d2f3f172b51957db6c1edd169e6a7305c3308bd
Deanrobert-dell <dean.peterson@ucas-edu.net>
2024-10-02 01:10:32 +0000

    gchjkghjgcvjhkgv

---
+++ conditionals/age.py
@@ -0,0 +1,12 @@
+age=input ("whats your age:")
+ageg=int(age)
+if ageg >= 18:
+    print ("you can vote yippeee")
+elif ageg >= 16:
+    print ("You can drive Yipeee")
+elif ageg >= 15:
+    print ("You can get a driving permit so close")
+elif ageg >= 5:
+    print ("you can go to school boooo")
+else:
+    print ("Are you a baby?")
