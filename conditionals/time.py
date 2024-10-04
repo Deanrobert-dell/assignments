7ab0ba180bfc71bad6420e91d277e7f4911170f3
Deanrobert-dell <dean.peterson@ucas-edu.net>
2024-10-04 19:29:01 +0000

    dsadasdsa

--- conditionals/time.py
+++ conditionals/time.py
@@ -3,3 +3,12 @@ import datetime
 curenttime = datetime.datetime.now()
 
 print(curenttime.hour)
+def main():
+    if curenttime.hour<12:
+        print("Wake up sleepy head")
+    elif curenttime.hour<18:
+        print("good afternoon")
+    else:
+        print("Go to sleep")
+main()
+
