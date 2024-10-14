240d8128acd7075f28c07d2262f4f9119ed30076
Deanrobert-dell <dean.peterson@ucas-edu.net>
2024-10-14 12:53:13 +0000

    afdsgfdsfghfgdv

---
+++ loops/fizzreal.py
@@ -0,0 +1,33 @@
+#make awesome fizzbuzz game
+
+
+num=1
+while num <= 50:
+  if num %5 ==0 and num %3 == 0:
+      print("fizzbuzz")
+  elif num %5 == 0:
+      print("buzz")
+  elif num %3 ==0:
+      print("fizz")
+#print numbers 1-50 
+  else: 
+    print(num)
+  num=num+1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
